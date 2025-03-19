d0acbc734f564a0e88da8977b45ec7a0e7b2093d
santiagorm <santiagorm@iris.com.co>
2025-03-19 09:56:09 -0500

    Adding email and change the ruta_principal path

--- Src/main.py
+++ Src/main.py
@@ -1,10 +1,48 @@
 import os
 import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
 from recorrerCarpetas import procesar_archivos_xlsm
 #from OrganizaciónDeCarpetas import rename_long_filenames
 
+
+
+def enviar_correo(ruta_archivo, destinatario, asunto, cuerpo):
+    remitente = 'santiagorm@iris.com.co'
+    password = 'vldy rhrm ywaa wsml'
+
+    # Crear el objeto del mensaje
+    mensaje = MIMEMultipart()
+    mensaje['From'] = remitente
+    mensaje['To'] = destinatario
+    mensaje['Subject'] = asunto
+
+    # Adjuntar el cuerpo del mensaje
+    mensaje.attach(MIMEText(cuerpo, 'plain'))
+
+    # Adjuntar el archivo
+    adjunto = MIMEBase('application', 'octet-stream')
+    with open(ruta_archivo, 'rb') as archivo:
+        adjunto.set_payload(archivo.read())
+    encoders.encode_base64(adjunto)
+    adjunto.add_header('Content-Disposition', f'attachment; filename={os.path.basename(ruta_archivo)}')
+    mensaje.attach(adjunto)
+
+    # Enviar el correo
+    with smtplib.SMTP('smtp.gmail.com', 587) as servidor:
+        servidor.starttls()
+        servidor.login(remitente, password)
+        servidor.send_message(mensaje)
+
+
+
+
 def main():
-    ruta_principal = 'CarpetasPlantillasDesgloce'
+
+    ruta_principal = 'todo\CarpetasPlantillasDesgloce0.0.1'
 
 
     df_stakeholders_general = procesar_archivos_xlsm(ruta_principal)
@@ -26,6 +64,21 @@ def main():
     with pd.ExcelWriter(ruta_xlsm, engine='openpyxl') as writer:
         df_stakeholders_general.to_excel(writer, index=False, sheet_name='Datos')
     print(f'DataFrame guardado en {ruta_xlsm}')
+    
+
+
+    body= f"""Buenas tardes.
+
+Se adjunta el archivo con el desglose de accionistas para su revisión.
+
+Este es un mensaje automático, por favor no responder a este correo.
+Quedamos atentos ante cualquier consulta.
+Saludos
+                """
+    enviar_correo("Resultado\\resultado desgloce accionistas.xlsx", "santiagorm@iris.com.co", "Envío de archivo – Desglose de Accionistas", body)
+
+
+
 
 # Ejecutar el método main solo si el script es ejecutado directamente
 if __name__ == '__main__':
